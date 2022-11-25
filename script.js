44c5841d6c834918a221e44639dfe16510b12b8d
alitolibov <109537573+alitolibov@users.noreply.github.com>
2022-11-25 17:04:06 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,206 @@
+let arr = [
+    {
+        "id": 1,
+        "title": "Кресло компьютерное игровое Cougar ARMOR One Black-Orange",
+        "description": "Cougar Armor One — игровое компьютерное кресло с высокой спинкой, поддерживающей позвоночник в анатомически правильном положении. Обивка сделана из воздухопроницаемой экокожи. В комплекте есть подушки для шеи и поясницы. За счет стального каркаса эта модель выдерживает нагрузку до 120 кг. Стул оснащен механизмом регулировки высоты «Газлифт». Металлический кожух защищает его от преждевременной поломки. Специальные ручки предназначены для настройки степени сопротивления к раскачиванию и его полной блокировки. Конструкция раскладывается на 180°. Подлокотники поворачиваются вправо и влево, их можно поднимать и опускать.",
+        "colors": [
+          "black",
+          "orange"
+        ],
+        "rating": 4.1,
+        "price": 20999,
+        "isBlackFriday": false,
+        "salePercentage": 0,
+        "media": [
+          "https://img.mvideo.ru/Big/50138276bb.jpg"
+        ],
+        "type": "furniture"
+      },
+    {
+      "id": 11,
+      "title": "Системный блок игровой HYPERPC Concept 5",
+      "description": "Необычная форма HyperPC Concept 5 говорит сама за себя – перед тобой не просто системный блок, а настоящая машина, способная запускать любые игры без тормозов. Продвинутая начинка позволяет насладиться главными хитами сезона и выставить максимальные настройки. НА ТОП-УРОВНЕ Компьютер от HyperPC использует восьмиядерный процессор Intel Core i7 11700KF и 16 Гб оперативной памяти, чего с лихвой",
+      "colors": [
+        "black"
+      ],
+      "rating": 4.7,
+      "price": 391999,
+      "isBlackFriday": false,
+      "salePercentage": 46,
+      "media": [
+        "https://img.mvideo.ru/Big/30058106bb.jpg",
+        "https://img.mvideo.ru/Big/30058106bb1.jpg",
+        "https://img.mvideo.ru/Big/30058106bb3.jpg"
+      ],
+      "type": "PC"
+    },
+    {
+      "id": 0.8474365959595023,
+      "title": "Телевизор Haier 55 Smart TV MX",
+      "description": "Диагональ экрана телевизора Haier 55 Smart TV MX — 55 дюймов, разрешение — 3840 х 2160 пикселей. Предусмотрены цифровое шумоподавление и масштабирование изображения до формата 4K. Есть поддержка HDR10. Акустическая система NICAM состоит из двух динамиков мощностью по 10 Вт. Для дистанционного управления предусмотрен инфракрасный пульт, также можно использовать смартфон с мобильным приложением. Эта модель работает под управлением операционной системы Android 9.0. Есть функции Smart TV, TimeShift и Sleep-таймер. Можно записывать фильмы и передачи на USB-носители и воспроизводить контент с флешек и портативных жестких дисков.",
+      "colors": [
+        "black"
+      ],
+      "dioganal": [
+        "32\"",
+        "43\"",
+        "50\"",
+        "55\"",
+        "75\""
+      ],
+      "rating": 3.9,
+      "price": 39999,
+      "isBlackFriday": false,
+      "salePercentage": 50,
+      "media": [
+        "https://img.mvideo.ru/Big/small_pic/65/10030234bb.jpg",
+        "https://img.mvideo.ru/Big/small_pic/65/10030234bb1.jpg",
+        "https://img.mvideo.ru/Big/small_pic/65/10030234bb2.jpg"
+      ],
+      "type": "TV"
+    },
+    {
+      "id": 21,
+      "title": "Игровой микрофон для компьютера Blue Yeti Nano Shadow Grey (988-000205)",
+      "description": "Для качественных трансляций в стриминговых сервисах наличия компьютера и горячего желания недостаточно. Микрофон Blue Yeti Nano позволит проводить стримы, записывать ролики и подкасты с отменным звуком.",
+      "colors": [
+        "grey"
+      ],
+      "rating": 5,
+      "price": 9999,
+      "isBlackFriday": false,
+      "salePercentage": 0,
+      "media": [
+        "https://img.mvideo.ru/Big/50159382bb.jpg"
+      ],
+      "type": "audio"
+    },
+    {
+      "id": 32,
+      "title": "Стиральная машина узкая Haier HW70-BP12969AS",
+      "description": "Глубина стиральной машины Haier HW70-BP12969AS всего 37,5 см, что делает её удачным выбором для установки в небольшом помещении. При скромных габаритах этой модели её барабан вмещает до 7 кг белья – подходящее решение для семьи из трёх-четырёх человек. Большой люк диаметром 52,5 см облегчает загрузку и выемку объёмных вещей: постельных принадлежностей, пледов, покрывал. ОБШИРНОЕ «МЕНЮ»«Хлопок», «Шерсть», «Синтетика» – для тканей разных видов предназначены свои программы. Кроме того, есть «Деликатный» цикл для шёлка, вискозы и тому подобных требовательных в уходе материалов. Специальные режимы разработаны для детской одежды и постельного белья. «Обработка паром» поможет устранить бактерии, избавит от неприятных запахов, освежит вещи после сезонного хранения. А функция «Дополнительное полоскание» удалит все остатки моющих средств и особенно пригодится семьям с детьми и аллергикам.",
+      "colors": [
+        "white",
+        "#D0D0D1FF"
+      ],
+      "rating": 4.4,
+      "price": 40999,
+      "isBlackFriday": false,
+      "salePercentage": 0,
+      "media": [
+        "https://img.mvideo.ru/Big/20064638bb.jpg",
+        "https://img.mvideo.ru/Big/20064638bb1.jpg",
+        "https://img.mvideo.ru/Big/20064638bb2.jpg",
+        "https://img.mvideo.ru/Big/20064638bb3.jpg"
+      ],
+      "type": "kitchen"
+    },
+    {
+      "id": 0.011139933448344408,
+      "title": "Телевизор Hisense 32A4BG",
+      "description": "Hisense 32A4BG — телевизор с 32-дюймовым экраном, разрешение которого составляет 1366 х 768 пикселей, а яркость — 180 кд/кв. м. За воспроизведение звука отвечают два динамика, их суммарная мощность — 12 Вт. В комплекте есть подставка, а для установки на стене надо дополнительно купить крепление типа VESA. Эта модель оснащена тюнером, принимающим сигнал цифрового ТВ в форматах DVB C/S/S2/T/T2. Выбор доступного контента расширяется благодаря поддержке Smart TV: устройство можно подключить к интернету по Wi-Fi или через разъем LAN. DLNA позволяет сделать его частью локальной сети. Для взаимодействия с внешними девайсами и накопителями данных есть порты HDMI и USB.",
+      "colors": [
+        "white",
+        "black"
+      ],
+      "dioganal": [
+        "32\"",
+        "40\""
+      ],
+      "rating": 4.8,
+      "price": 12499,
+      "isBlackFriday": true,
+      "salePercentage": 26,
+      "media": [
+        "https://img.mvideo.ru/Big/10030407bb.jpg",
+        "https://img.mvideo.ru/Big/small_pic/65/10030407bb1.jpg",
+        "https://img.mvideo.ru/Big/small_pic/65/10030407bb3.jpg"
+      ],
+      "type": "TV"
+    }
+]
+let products = document.querySelector('.productss')
+for (let item of arr) {
+    let countOfRating = Math.round(item.rating);
+    let discount = item.price - (item.salePercentage * item.price) / 100;
+
+    let box = document.createElement("div");
+    let box_top = document.createElement("div");
+    let p_of_top = document.createElement("p");
+    let div_strelka = document.createElement("div");
+    let img = document.createElement("img");
+    let title = document.createElement("div");
+    let reating = document.createElement("div");
+    let reating_star = document.createElement("img");
+    let reating_num = document.createElement("p");
+    let reating_count = document.createElement("p");
+    let price = document.createElement("div");
+    let current_price = document.createElement("p");
+    let old_price = document.createElement("p");
+    let sth = document.createElement("img");
+    let box_bot = document.createElement("div");
+    let btn_bot = document.createElement("button");
+    let like = document.createElement("img");
+    let chart = document.createElement("img");
+
+    box.classList.add("box");
+    box_top.classList.add("box_top");
+    p_of_top.classList.add("p_of_top");
+    div_strelka.classList.add("div_strelka");
+    img.classList.add("title-img");
+    title.classList.add("title");
+    reating.classList.add("reating");
+    reating_star.classList.add("reating_star");
+    reating_num.classList.add("reating_num");
+    reating_count.classList.add("reating_count");
+    price.classList.add("price");
+    current_price.classList.add("current_price");
+    old_price.classList.add("old_price");
+    sth.classList.add("sth");
+    box_bot.classList.add("box_bot");
+    btn_bot.classList.add("def-btn");
+    like.classList.add("like");
+    chart.classList.add("chart");
+
+    if (item.salePercentage == 0) {
+      old_price.style.display = "none";
+    }
+    img.src = item.media[0];
+
+    title.innerHTML = item.title;
+
+    reating_star.src = "./public/icons/star.svg";
+    reating_num.innerHTML = item.rating;
+    reating_count.innerHTML = `${countOfRating} отзывов`;
+
+    current_price.innerHTML = discount + "$";
+    old_price.innerHTML = item.price;
+    sth.src = "./public/icons/sth.png";
+    btn_bot.innerHTML = `<img src="./public/icons/shopping-cart.svg">`;
+    like.src = "./public/icons/heart.svg";
+    chart.src = "./public/icons/bar-chart-2.svg";
+
+    reating.append(reating_star, reating_num, reating_count);
+    price.append(current_price, old_price);
+    box_bot.append(btn_bot, like, chart);
+
+    box.append(img, title, reating, price, sth, box_bot);
+    products.append(box);
+
+    img.onclick = () => {
+      openProduct(item);
+    };
+    title.onclick = () => {
+      openProduct(item);
+    };
+  }
+  var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
